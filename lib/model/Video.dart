32392c55c1f0c9684500b080f62790cb16850078
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-24 23:51:19 +0100

    model video

---
+++ lib/model/Video.dart
@@ -0,0 +1,31 @@
+class Video {
+  String id;
+  String titulo;
+  String descricao;
+  String imagem;
+  String canal;
+
+  //Construtor
+  Video({this.id, this.titulo, this.descricao, this.imagem, this.canal});
+
+  //Getters e Setters
+  String get getId => this.id;
+
+  set setId(String id) => this.id = id;
+
+  get getTitulo => this.titulo;
+
+  set setTitulo(titulo) => this.titulo = titulo;
+
+  get getDescricao => this.descricao;
+
+  set setDescricao(descricao) => this.descricao = descricao;
+
+  get getImagem => this.imagem;
+
+  set setImagem(imagem) => this.imagem = imagem;
+
+  get getCanal => this.canal;
+
+  set setCanal(canal) => this.canal = canal;
+}
