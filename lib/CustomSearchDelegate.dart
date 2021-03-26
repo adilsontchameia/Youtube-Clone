c37e7410511e8530434df6380c28f13cec846913
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-27 00:27:37 +0100

    apagando o que esta escrito

--- lib/CustomSearchDelegate.dart
+++ lib/CustomSearchDelegate.dart
@@ -7,7 +7,12 @@ class CustomSearchDelegate extends SearchDelegate<String> {
   List<Widget> buildActions(BuildContext context) {
     return [
       //Botao de apagar
-      IconButton(icon: Icon(Icons.clear), onPressed: () {}),
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            //Pegar o que esta ser digitado
+            query = "";
+          }),
       //IconButton(icon: Icon(Icons.done), onPressed: () {}),
     ];
   }
@@ -15,7 +20,12 @@ class CustomSearchDelegate extends SearchDelegate<String> {
   @override
   //Metodo que define o botao de voltar
   Widget buildLeading(BuildContext context) {
-    return Container();
+    return IconButton(
+        icon: Icon(Icons.arrow_back_ios),
+        onPressed: () {
+          //Quando fechamos, podemos passar um parametro depois do context
+          close(context, null);
+        });
   }
 
   //Controla os resultados pesquisados
